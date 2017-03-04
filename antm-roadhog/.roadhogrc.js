1b8e82e30a198bae528a2f1d01e25cb1f7f9a702
paranoidjk <hust2012jiangkai@gmail.com>
2017-03-04 12:50:47 +0800

    feat: add demo with roadhog

---
+++ antm-roadhog/.roadhogrc.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const pxtorem = require('postcss-pxtorem');
+
+const svgSpriteDirs = [
+  require.resolve('antd-mobile').replace(/warn\.js$/, ''), // antd-mobile 内置svg
+  path.resolve(__dirname, 'src/svg/'),  // 业务代码本地私有 svg 存放目录
+];
+
+export default {
+  "entry": "src/index.js",
+  svgSpriteLoaderDirs: svgSpriteDirs,
+  "env": {
+    "development": {
+      "extraBabelPlugins": [
+        "dva-hmr",
+        "transform-runtime",
+        ["import", { "libraryName": "antd-mobile", "libraryDirectory": "lib", "style": "css" }]
+      ],
+      extraPostCSSPlugins: [
+        pxtorem({
+          rootValue: 100,
+          propWhiteList: [],
+        }),
+      ],
+    },
+    "production": {
+      "extraBabelPlugins": [
+        "transform-runtime",
+        ["import", { "libraryName": "antd-mobile", "libraryDirectory": "lib", "style": "css" }]
+      ],
+      extraPostCSSPlugins: [
+        pxtorem({
+          rootValue: 100,
+          propWhiteList: [],
+        }),
+      ],
+    }
+  }
+}
---
+++ antm-roadhog/README.md
@@ -0,0 +1,13 @@
+## antd-mobile demo with roadhog, dva
+
+svg config + rem config
+
+### dev
+
+```
+npm install
+npm start
+```
+
+http://localhost:8000/
+
