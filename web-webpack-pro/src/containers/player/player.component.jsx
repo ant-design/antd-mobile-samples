ceb2f4b378253b131eee9785a9557e443047ee88
paranoidjk <hust2012jiangkai@gmail.com>
2017-03-08 21:55:42 +0800

    feat: add demo

--- web-webpack-pro/src/containers/player/player.component.jsx
+++ web-webpack-pro/src/containers/player/player.component.jsx
@@ -1,6 +1,44 @@
 import React from 'react'
 import { observer } from 'mobx-react'
 
+import { List, Checkbox, Flex } from 'antd-mobile';
+
+const CheckboxItem = Checkbox.CheckboxItem;
+const AgreeItem = Checkbox.AgreeItem;
+
+class Test extends React.Component {
+  onChange = (val) => {
+    console.log(val);
+  }
+  render() {
+    const data = [
+      { value: 0, label: '博士' },
+      { value: 1, label: '本科' },
+      { value: 2, label: '高中' },
+    ];
+    return (<div>
+      <List renderHeader={() => 'CheckboxItem 演示'}>
+        {data.map(i => (
+          <CheckboxItem key={i.value} onChange={() => this.onChange(i.value)}>
+            {i.label}
+          </CheckboxItem>
+        ))}
+        <CheckboxItem key="disabled" data-seed="logId" disabled defaultChecked multipleLine>
+          初中<List.Item.Brief>辅助文字内容</List.Item.Brief>
+        </CheckboxItem>
+      </List>
+
+      <Flex>
+        <Flex.Item>
+          <AgreeItem data-seed="logId" onChange={e => console.log('checkbox', e)}>
+            已阅读协议<a onClick={(e) => { e.preventDefault(); alert('打开协议'); }}>《协议链接》</a>
+          </AgreeItem>
+        </Flex.Item>
+      </Flex>
+    </div>);
+  }
+}
+
 @observer
 class Player extends React.Component {
     constructor(props) {
@@ -15,9 +53,14 @@ class Player extends React.Component {
         return (
             <div className='player'>
                 <div>Player</div>
+                <Test />
             </div>
         )
     }
 }
 
 export default Player
+
+
+
+
