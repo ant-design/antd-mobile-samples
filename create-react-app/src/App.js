fb636fc32d5103603bef0f280eff7f3c540c1ca2
silentcloud <rjmuqiang@gmail.com>
2017-06-01 10:26:59 +0800

    add create-react-app sample

---
+++ create-react-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Button } from 'antd-mobile';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button>This is a button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
