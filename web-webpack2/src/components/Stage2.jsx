230616722e5a55ebcd93d3e3706dd769de4e71c8
然则 <hualei.hl@alibaba-inc.com>
2017-09-12 18:17:25 +0800

    fix web-webpack2 demo

--- web-webpack2/src/components/Stage2.jsx
+++ web-webpack2/src/components/Stage2.jsx
@@ -14,19 +14,15 @@ export default class Demo extends React.Component {
 
       <SearchBar placeholder="搜索" />
 
-      <Tabs defaultActiveKey="1">
-        <TabPane tab="选项卡一" key="1">
-          <Steps direction="horizontal">
-            <Steps.Step title="现在" description="立即买入" />
-            <Steps.Step title="11月3日" description="买入成功" />
-            <Steps.Step title="11月4日" description="收益到账" />
-          </Steps>
-        </TabPane>
-        <TabPane tab="选项卡二" key="2">
-          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: 2000 }}>
-            选项卡二内容，内容很长，测试向下滑动页面，是否会导致 tab content 内容的左右偏移
-          </div>
-        </TabPane>
+      <Tabs tabs={[{ title: '选项卡一' }, { title: '选项卡二' }]} renderTab={tab => tab.title}>
+        <Steps direction="horizontal">
+          <Steps.Step title="现在" description="立即买入" />
+          <Steps.Step title="11月3日" description="买入成功" />
+          <Steps.Step title="11月4日" description="收益到账" />
+        </Steps>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: 2000 }}>
+          选项卡二内容，内容很长，测试向下滑动页面，是否会导致 tab content 内容的左右偏移
+        </div>
       </Tabs>
     </div>);
   }
