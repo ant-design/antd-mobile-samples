50f99bd53633cc400b0ac67f29b662e466d252ad
然则 <hualei.hl@alibaba-inc.com>
2017-09-21 11:16:50 +0800

    up web-custom-ui-pro

--- web-custom-ui-pro/index.js
+++ web-custom-ui-pro/index.js
@@ -1,9 +1,37 @@
-// Just import style for https://github.com/ant-design/ant-design/issues/3745
-const req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
+/**
+ * This is the entry file for `npm run dist`
+ */
 
-// build for `dist/antd-mobile.css`
+/**
+ * build for `dist/antd-mobile.css`
+ * Note: Include css
+ */
+// const req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
+const req = require.context('./components', true, /^\.\/[^_][\w-]+\/(style\/)?index\.tsx?$/);
 req.keys().forEach((mod) => {
-  req(mod);
+  let v = req(mod);
+  if (v && v.default) {
+    v = v.default;
+  }
+  const match = mod.match(/^\.\/([^_][\w-]+)\/index\.tsx?$/);
+  if (match && match[1]) {
+    exports[camelCase(match[1])] = v;
+  }
 });
 
+/**
+ * build for `dist/antd-mobile.js`
+ */
 // module.exports = require('./components/index');
+
+function camelCase(name) {
+  return name.charAt(0).toUpperCase() +
+    name.slice(1).replace(/-(\w)/g, (m, n) => {
+      return n.toUpperCase();
+    });
+}
+/* eslint no-console:0 */
+if (typeof console !== 'undefined' && console.warn) {
+  console.warn(`
+Please use https://github.com/ant-design/babel-plugin-import to reduce app bundle size.`);
+}
