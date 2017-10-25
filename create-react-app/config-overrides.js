644c449ac427411f0a529584b15679e5c6779bc7
silentcloud <rjmuqiang@gmail.com>
2017-10-25 16:52:42 +0800

    feat: add css-modules for create-react-app, close ant-design/ant-design-mobile#1990

--- create-react-app/config-overrides.js
+++ create-react-app/config-overrides.js
@@ -1,6 +1,47 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = function override(config, env) {
+  // babel-plugin-import
   config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }], config);
+
+  // css-modules
+  config.module.rules[1].oneOf.unshift(
+    {
+      test: /\.css$/,
+      exclude: /node_modules|antd-mobile\.css/,
+      use: [
+        require.resolve('style-loader'),
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: '[local]___[hash:base64:5]'
+          },
+        },
+        {
+          loader: require.resolve('postcss-loader'),
+          options: {
+            // Necessary for external CSS imports to work
+            // https://github.com/facebookincubator/create-react-app/issues/2677
+            ident: 'postcss',
+            plugins: () => [
+              require('postcss-flexbugs-fixes'),
+              autoprefixer({
+                browsers: [
+                  '>1%',
+                  'last 4 versions',
+                  'Firefox ESR',
+                  'not ie < 9', // React doesn't support IE8 anyway
+                ],
+                flexbox: 'no-2009',
+              }),
+            ],
+          },
+        },
+      ]
+    }
+  );
+
   return config;
 };
--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Icon } from 'antd-mobile';
-import './App.css';
+import styles from './App.css';
 
 const customIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3">
@@ -15,13 +15,13 @@ const customIcon = () => (
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className={styles.App}>
+        <div className={styles['App-header']}>
           <Icon type="success" size="lg"/>
           <div style={{ width: 60, height:60 }}>{customIcon()}</div>
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className={styles['App-intro']}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Button type="primary">This is a button</Button>
