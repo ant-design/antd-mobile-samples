109f622e3504f5c7f6ccccd7328be02c7e546051
silentcloud <rjmuqiang@gmail.com>
2017-10-26 10:51:40 +0800

    feat: add custom theme for create-react-app, close ant-design/ant-design-mobile#1997

--- create-react-app/config-overrides.js
+++ create-react-app/config-overrides.js
@@ -1,8 +1,54 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const { injectBabelPlugin, getLoader } = require('react-app-rewired');
+
+const fileLoaderMatcher = function (rule) {
+  return rule.loader && rule.loader.indexOf(`file-loader`) != -1;
+}
 
 module.exports = function override(config, env) {
   // babel-plugin-import
-  config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }], config);
+  config = injectBabelPlugin(['import', {
+    libraryName: 'antd-mobile',
+    //style: 'css',
+    style: true, // use less for customized theme
+  }], config);
+
+  // customize theme
+  config.module.rules[1].oneOf.unshift(
+    {
+      test: /\.less$/,
+      use: [
+        require.resolve('style-loader'),
+        require.resolve('css-loader'),
+        {
+          loader: require.resolve('postcss-loader'),
+          options: {
+            // Necessary for external CSS imports to work
+            // https://github.com/facebookincubator/create-react-app/issues/2677
+            ident: 'postcss',
+            plugins: () => [
+              require('postcss-flexbugs-fixes'),
+              autoprefixer({
+                browsers: [
+                  '>1%',
+                  'last 4 versions',
+                  'Firefox ESR',
+                  'not ie < 9', // React doesn't support IE8 anyway
+                ],
+                flexbox: 'no-2009',
+              }),
+            ],
+          },
+        },
+        {
+          loader: require.resolve('less-loader'),
+          options: {
+            // theme vars, also can use theme.js instead of this.
+            modifyVars: { "@brand-primary": "#1DA57A" },
+          },
+        },
+      ]
+    }
+  );
 
   // css-modules
   config.module.rules[1].oneOf.unshift(
@@ -42,6 +88,10 @@ module.exports = function override(config, env) {
       ]
     }
   );
+
+  // file-loader exclude
+  let l = getLoader(config.module.rules, fileLoaderMatcher);
+  l.exclude.push(/\.less$/);
 
   return config;
 };
